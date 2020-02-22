74999bed95199acda679943e6fa7f639013cfa76
Timothee Boingneres <timboing@icloud.com>
2020-02-22 12:25:44 +0100

    ok cest pas mal meme au niveau css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,6 @@ class PagesController < ApplicationController
   def index
     @url = "https://fr.wikipedia.org/wiki/wagon"
     @html_doc = Nokogiri::HTML(open(@url), nil, 'utf-8')
+    @doc_body = @html_doc.search('body')
   end
 end
